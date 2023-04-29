9b771ab2e5b2a0d948cc18208820b235d3a2d64f
Mikołaj Szuper <mikisuper09@gmail.com>
2023-04-29 21:18:03 +0200

    Menu gry

--- PAKOCarChase2/PAKOCarChase2.cpp
+++ PAKOCarChase2/PAKOCarChase2.cpp
@@ -5,22 +5,74 @@
 #include "board.h"
 #include "obstacle.h"
 #include "ai.h"
+#include "button.h"
 
 using namespace sf;
+
+RenderWindow app(VideoMode(640, 480), "Pako Car Game");
+void NewGame();
+
 int main()
-{
-    RenderWindow app(VideoMode(640, 480), "Pako Car Game");
+{    
     app.setFramerateLimit(60);
+
+    sf::Sprite logo;
+    sf::Texture tex_logo;
+    tex_logo.loadFromFile("assets/logo.png");
+    logo.setTexture(tex_logo);
+    logo.setOrigin(193,96.5);
+    logo.setPosition(sf::Vector2f(app.getSize().x / 2, (app.getSize().y / 2)-125));
+    logo.scale(0.6,0.6);
+
+    Button buttonPlay(app, "Play", sf::Vector2f(app.getSize().x / 2, (app.getSize().y / 2)));
+    Button buttonWhoBest(app, "Records", sf::Vector2f(app.getSize().x / 2, (app.getSize().y / 2)+50));
+    Button buttonExit(app, "Exit", sf::Vector2f(app.getSize().x / 2, (app.getSize().y / 2) + 100));
+    while (app.isOpen())
+    {
+        Event e;
+        while (app.pollEvent(e))
+        {
+            if (e.type == Event::Closed) {
+                app.close();
+            }
+        }
+
+        app.clear(sf::Color(80, 80, 80));
+        buttonPlay.update(app);
+        buttonWhoBest.update(app);
+        buttonExit.update(app);
+        app.draw(logo);
+        app.display();
+
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            if (buttonPlay.isHover()) {
+                NewGame();
+            }
+            else if (buttonWhoBest.isHover())
+            {
+                std::cout << "Records!" << std::endl;
+            }
+            else if(buttonExit.isHover())
+            {
+                return EXIT_SUCCESS;
+            }
+        }
+
+    }
+    return EXIT_SUCCESS;
+}
+
+void NewGame() {
     Board map("assets/grass.png");
-    Player player("assets/car.png",Vector2f(app.getSize().x/2, app.getSize().y/2), map.getBorder());
+    Player player("assets/car.png", Vector2f(app.getSize().x / 2, app.getSize().y / 2), map.getBorder());
     Ai player1("assets/police.png", map.getBorder());
     srand(time(NULL));
     Obstacle obs[10];
     int j = 0;
-    while (j<10)
+    while (j < 10)
     {
         int k = 1 + j;
-        while (k<10)
+        while (k < 10)
         {
             if (obs[j].getObstacle().getGlobalBounds().intersects(obs[k].getObstacle().getGlobalBounds()) || obs[j].getObstacle().getGlobalBounds().intersects(player.getPlayer().getGlobalBounds())) {
                 obs[j].regenarateObstacle();
@@ -49,9 +101,9 @@ int main()
 
         Obstacle* wsk_obs = *&obs;
 
-        player.update(map.getMap(),wsk_obs);
+        player.update(map.getMap(), wsk_obs);
 
-        player1.update(map.getMap(),wsk_obs,player.getPosToPolice());
+        player1.update(map.getMap(), wsk_obs, player.getPosToPolice());
 
         for (int i = 0; i < 10; i++)
         {
@@ -77,6 +129,4 @@ int main()
         //app.draw(player.getSensor());
         app.display();
     }
-
-    return EXIT_SUCCESS;
 }
